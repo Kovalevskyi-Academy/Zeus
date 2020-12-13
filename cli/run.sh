9617375c42ca3f35d065b24ea13ba4924134df6e
Viacheslav Kovalevskyi <viacheslav@kovalevskyi.com>
2020-12-12 17:56:34 -0800

    updated execution logic

--- cli/run.sh
+++ cli/run.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
 
-java -cp "Zeus-1.0-jar-with-dependencies.jar:CodingBootcamp-1.0.jar" com.kovalevskyi.academy.codingbootcamp.suite.Zeus --week 0 --day 0
+java -cp "../../CodingBootcamp/target/CodingBootcamp-1.0.jar:Zeus.jar" com.kovalevskyi.academy.codingbootcamp.suite.Zeus --week 0 --day 0
