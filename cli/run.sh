cc8c21880c70da3d61b6a2930d880e6626a65fc2
Vladyslav <ecstasy.awesome@gmail.com>
2020-12-17 12:00:28 +0200

    added windows/unix-like commands to start Zeus

--- cli/run.sh
+++ cli/run.sh
@@ -2,4 +2,10 @@
 
 java -cp "../CodingBootcamp/target/CodingBootcamp-1.0.jar:cli/Zeus.jar" com.kovalevskyi.academy.codingbootcamp.suite.Zeus --maven /Users/vkovalevskyi/dev/apache-maven-3.6.3 --week 0 --day 0
 java -cp "Zeus.jar" com.kovalevskyi.academy.codingbootcamp.suite.Zeus --maven /Users/vkovalevskyi/dev/apache-maven-3.6.3 --week 0 --day 0
+
+
+# windows
 java -cp "CodingBootcamp-1.0.jar;Zeus.jar" com.kovalevskyi.academy.codingbootcamp.suite.Zeus -w0 -d1
+
+# unix-like
+java -cp "CodingBootcamp-1.0.jar:Zeus.jar" com.kovalevskyi.academy.codingbootcamp.suite.Zeus -w0 -d1
