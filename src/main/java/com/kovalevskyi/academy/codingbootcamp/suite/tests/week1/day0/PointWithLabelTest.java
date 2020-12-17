a95cb4cc4248a9805e8a97129f62b1758aabbe98
Viacheslav Kovalevskyi <viacheslav@kovalevskyi.com>
2020-12-16 21:07:02 -0800

    small new test for PointWithLabel added

---
+++ src/main/java/com/kovalevskyi/academy/codingbootcamp/suite/tests/week1/day0/PointWithLabelTest.java
@@ -0,0 +1,44 @@
+package com.kovalevskyi.academy.codingbootcamp.suite.tests.week1.day0;
+
+import com.kovalevskyi.academy.codingbootcamp.suite.AbstractTestExecutor;
+import com.kovalevskyi.academy.codingbootcamp.week0.day3.Point;
+import com.kovalevskyi.academy.codingbootcamp.week1.day0.PointWithLabel;
+import org.junit.jupiter.api.Test;
+
+import static com.google.common.truth.Truth.assertWithMessage;
+
+public class PointWithLabelTest extends AbstractTestExecutor {
+
+  @Test
+  public void testGetLabel() {
+    var inputValue = "myLabel";
+
+    var point = new PointWithLabel(1, 2, inputValue);
+
+    assertWithMessage(
+            String.format(
+                "checking if new PointWithLabel(1, 2, myLabel)" + " will return myLabel correctly"))
+        .that(point.getLabel())
+        .isEqualTo(inputValue);
+    assertWithMessage("Checking is test point also is " + "Point.class")
+        .that(point)
+        .isInstanceOf(Point.class);
+  }
+
+  @Test
+  public void testCompareTo() {
+      var x1 = 1;
+      var x2 = 2;
+      var y1 = 0;
+      var y2 = 2;
+      var x3 = 3;
+      var y3 = 4;
+
+      var point1 = new PointWithLabel(x1, y1, "hi");
+      var point2 = new PointWithLabel(x2, y2, "ololo");
+      var point3 = new Point(x3, y3);
+
+    assertWithMessage("assesrting that (1, 0, \"hi\") compareTo with Point(3, 4) will return 6").that(point1.compareTo(point3))
+        .isEqualTo(6);
+  }
+}
