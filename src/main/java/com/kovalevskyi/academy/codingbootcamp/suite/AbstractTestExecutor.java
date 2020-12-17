779eece5e21b400fcadebe8e8d06c6c2c1584673
Vladyslav <ecstasy.awesome@gmail.com>
2020-12-17 11:56:32 +0200

    deleted a local unused variable

--- src/main/java/com/kovalevskyi/academy/codingbootcamp/suite/AbstractTestExecutor.java
+++ src/main/java/com/kovalevskyi/academy/codingbootcamp/suite/AbstractTestExecutor.java
@@ -4,7 +4,6 @@ import static org.junit.platform.engine.discovery.DiscoverySelectors.selectClass
 
 import org.junit.platform.launcher.Launcher;
 import org.junit.platform.launcher.LauncherDiscoveryRequest;
-import org.junit.platform.launcher.TestPlan;
 import org.junit.platform.launcher.core.LauncherDiscoveryRequestBuilder;
 import org.junit.platform.launcher.core.LauncherFactory;
 import org.junit.platform.launcher.listeners.SummaryGeneratingListener;
@@ -18,7 +17,7 @@ public abstract class AbstractTestExecutor {
         .selectors(selectClass(this.getClass()))
         .build();
     Launcher launcher = LauncherFactory.create();
-    TestPlan testPlan = launcher.discover(request);
+    launcher.discover(request);
     launcher.registerTestExecutionListeners(listener);
     launcher.execute(request);
   }
