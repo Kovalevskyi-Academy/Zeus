bd4ff282489fd92208723261e26f6e29e6444dda
Vladyslav <ecstasy.awesome@gmail.com>
2020-12-19 16:22:26 +0200

    added simple solution to run checkstyle

---
+++ src/main/java/com/kovalevskyi/academy/codingbootcamp/suite/CheckstyleUtil.java
@@ -0,0 +1,24 @@
+package com.kovalevskyi.academy.codingbootcamp.suite;
+
+import com.puppycrawl.tools.checkstyle.Main;
+import java.io.IOException;
+
+public class CheckstyleUtil {
+
+  public enum Checkstyle {
+
+    GOOGLE_CHECKS("google_checks.xml"),
+    SUN_CHECKS("sun_checks.xml");
+
+    public final String checksFile;
+
+    Checkstyle(String checksFile) {
+      this.checksFile = checksFile;
+    }
+
+  }
+
+  public static void runCheckstyle(Checkstyle checkstyle, String testedClass) throws IOException {
+    Main.main("-c", checkstyle.checksFile, testedClass);
+  }
+}
