c3cd8f2f0645deda97ed1e26e1be5d46cf313896
seralekseenko <seralekseenko@ukr.net>
2021-01-21 01:50:57 +0200

    fix style

--- src/main/java/com/kovalevskyi/academy/zeus/TheMighty.java
+++ src/main/java/com/kovalevskyi/academy/zeus/TheMighty.java
@@ -62,7 +62,7 @@ public class TheMighty implements Callable<Integer> {
               "com.kovalevskyi.academy.codingbootcamp.week2.day1.TextPrinterTest"
           },
       }
-  };
+    };
 
   @CommandLine.Option(
       names = {"-w", "--week"},
