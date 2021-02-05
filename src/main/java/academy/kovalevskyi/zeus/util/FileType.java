1db6736821507c8652e1701278364f8842cb9476
Vladyslav <ecstasy.awesome@gmail.com>
2021-02-05 13:54:50 +0200

    add supported file types

---
+++ src/main/java/academy/kovalevskyi/zeus/util/FileType.java
@@ -0,0 +1,13 @@
+package academy.kovalevskyi.zeus.util;
+
+public enum FileType {
+
+  JAR(".jar"),
+  JAVA(".java");
+
+  public final String extension;
+
+  FileType(String extension) {
+    this.extension = extension;
+  }
+}
