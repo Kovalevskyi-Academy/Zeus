f04e67fab98d0b85158a86f09410fe0196d4b2ce
Vladyslav <ecstasy.awesome@gmail.com>
2021-02-05 13:56:38 +0200

    add base command

---
+++ src/main/java/academy/kovalevskyi/zeus/cli/command/Zeus.java
@@ -0,0 +1,30 @@
+package academy.kovalevskyi.zeus.cli.command;
+
+import academy.kovalevskyi.zeus.cli.VersionProvider;
+import academy.kovalevskyi.zeus.cli.command.sub.Checkstyle;
+import academy.kovalevskyi.zeus.cli.command.sub.Maven;
+import academy.kovalevskyi.zeus.cli.command.sub.Show;
+import academy.kovalevskyi.zeus.cli.command.sub.Test;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+
+@Command(
+    name = "Zeus",
+    mixinStandardHelpOptions = true,
+    versionProvider = VersionProvider.class,
+    footer = "%nCopyright (c) 2021",
+    subcommands = {Test.class, Show.class, Maven.class, Checkstyle.class}
+)
+public class Zeus implements Runnable {
+
+  @Option(names = {"-d", "--debug"}, description = "Debug mode")
+  private boolean debug;
+
+  @Override
+  public void run() {
+  }
+
+  public boolean isDebug() {
+    return debug;
+  }
+}
