0f996773bb3714fc7275ad4180a20c5565c13aa0
Viacheslav Kovalevskyi <viacheslav@kovalevskyi.com>
2021-02-06 17:21:19 -0800

    release 2.2

--- src/main/java/academy/kovalevskyi/zeus/cli/VersionProvider.java
+++ src/main/java/academy/kovalevskyi/zeus/cli/VersionProvider.java
@@ -9,8 +9,12 @@ public class VersionProvider implements IVersionProvider {
   private final Model mavenConfig = MavenEngine.getConfig();
 
   public String[] getVersion() {
-    return new String[]{
-        String.format("Name: %s", mavenConfig.getArtifactId()),
-        String.format("Version: %s", mavenConfig.getVersion())};
+//    return new String[]{
+//        String.format("Name: %s", mavenConfig.getArtifactId()),
+//        String.format("Version: %s", mavenConfig.getVersion())};
+    return new String[] {
+        "academy.kovalevskyi",
+        "2.2"
+    };
   }
 }
