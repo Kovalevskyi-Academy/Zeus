e8b1c17536ea75191e5c600424f9161dca351439
Vladyslav <ecstasy.awesome@gmail.com>
2021-02-15 19:25:30 +0200

    add new api of Testing Framework

--- src/main/java/academy/kovalevskyi/zeus/cli/command/sub/Test.java
+++ src/main/java/academy/kovalevskyi/zeus/cli/command/sub/Test.java
@@ -1,19 +1,18 @@
 package academy.kovalevskyi.zeus.cli.command.sub;
 
-import academy.kovalevskyi.testing.AbstractTestExecutor;
-import academy.kovalevskyi.testing.service.ContainerRequest;
-import academy.kovalevskyi.testing.util.CourseManager;
-import academy.kovalevskyi.testing.view.TestHandler;
+import academy.kovalevskyi.testing.util.ContainerLauncher;
 import academy.kovalevskyi.zeus.cli.group.CourseRequest;
 import academy.kovalevskyi.zeus.util.FileExplorer;
-import java.util.List;
 import java.util.concurrent.Callable;
 import picocli.CommandLine.ArgGroup;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 
-@Command(name = "test", description = "Run containers by course/week/day/id")
-public class Test implements Callable<Integer> {
+@Command(
+    name = "test",
+    description = "Run containers by course/week/day/id",
+    mixinStandardHelpOptions = true)
+public class Test implements Callable<Void> {
 
   @Option(names = {"-e", "--error"}, description = "Show only errors")
   private boolean error;
@@ -21,37 +20,12 @@ public class Test implements Callable<Integer> {
   @ArgGroup(exclusive = false, multiplicity = "1")
   private final CourseRequest request = new CourseRequest();
 
-  public Integer call() throws Exception {
+  public Void call() throws Exception {
     if (FileExplorer.isJarAbsentInClasspath()) {
       System.out.println("Add your jar file first into classpath, because classpath is empty!");
     } else {
-      TestHandler.setErrorMode(error);
-      var containers = (List<Class<? extends AbstractTestExecutor>>) null;
-      if (request.getId() >= 0) {
-        containers = CourseManager.getContainers(ContainerRequest
-            .builder()
-            .course(request.getCourse())
-            .week(request.getWeek())
-            .day(request.getDay())
-            .id(request.getId())
-            .build());
-
-      } else {
-        containers = CourseManager.getContainers(ContainerRequest
-            .builder()
-            .course(request.getCourse())
-            .week(request.getWeek())
-            .day(request.getDay())
-            .build());
-      }
-      for (var container : containers) {
-        execute(container);
-      }
+      ContainerLauncher.execute(error, request.prepareRequest());
     }
-    return 0;
-  }
-
-  private void execute(Class<? extends AbstractTestExecutor> container) throws Exception {
-    container.getConstructor().newInstance().execute();
+    return null;
   }
 }
