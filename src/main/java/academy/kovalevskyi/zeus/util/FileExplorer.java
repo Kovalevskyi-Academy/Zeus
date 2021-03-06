b6d0144ee18cb9914409db1684fd50d809c2ce80
Vladyslav <ecstasy.awesome@gmail.com>
2021-03-06 02:20:12 +0200

    improvements
    - add deep search to file explorer
    - optimize code

--- src/main/java/academy/kovalevskyi/zeus/util/FileExplorer.java
+++ src/main/java/academy/kovalevskyi/zeus/util/FileExplorer.java
@@ -7,70 +7,77 @@ import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class FileExplorer {
+public final class FileExplorer {
 
   public static final String M2_HOME;
   public static final String JAVA_CLASSPATH;
   public static final String JAVA_SOURCES;
   public static final String WORKING_DIRECTORY;
+  public static final String OUTPUT_DIRECTORY;
 
   static {
     WORKING_DIRECTORY = System.getProperty("user.dir");
     M2_HOME = System.getenv("M2_HOME");
     JAVA_CLASSPATH = System.getProperty("java.class.path");
-    JAVA_SOURCES = String.format("%s%2$ssrc%2$smain%2$sjava", WORKING_DIRECTORY, File.separator);
+    JAVA_SOURCES = String.format("%s%2$ssrc", WORKING_DIRECTORY, File.separator);
+    OUTPUT_DIRECTORY = String.format("%s%2$starget", WORKING_DIRECTORY, File.separator);
   }
 
   public static boolean match(final String fileName, final FileType type) {
     return fileName.toLowerCase().endsWith(type.extension);
   }
 
-  public static boolean isJarAbsentInClasspath() {
+  public static boolean isClasspathNotEmpty() {
     return Arrays
-        .stream(JAVA_CLASSPATH.split("[;|:]"))
+        .stream(JAVA_CLASSPATH.split(File.pathSeparator))
         .filter(file -> match(file, FileType.JAR))
         .distinct()
-        .count() == 1;
+        .count() > 1;
   }
 
-  public static List<File> getFiles(final File directory, final FileType type) {
-    return getFiles(directory.getAbsolutePath(), type);
+  public static List<File> getFiles(
+      final File directory,
+      final boolean deepSearch,
+      final FileType type) {
+    return getFiles(directory.getAbsolutePath(), deepSearch, type);
   }
 
-  public static List<File> getFiles(final String directory, final FileType type) {
-    return getFiles(directory)
+  public static List<File> getFiles(
+      final String directory,
+      final boolean deepSearch,
+      final FileType type) {
+    return getFiles(directory, deepSearch)
         .stream()
         .filter(file -> match(file.getName(), type))
         .collect(Collectors.toUnmodifiableList());
   }
 
-  public static List<File> getFiles(final File directory) {
-    return getFiles(directory.getAbsolutePath());
+  public static List<File> getFiles(final File directory, final boolean deepSearch) {
+    return getFiles(directory.getAbsolutePath(), deepSearch);
   }
 
-  public static List<File> getFiles(final String directory) {
-    var folder = new File(directory);
-    if (!folder.isDirectory()) {
-      throw new IllegalArgumentException(String.format("Wrong directory path '%s'", directory));
+  public static List<File> getFiles(final String directory, final boolean deepSearch) {
+    final var folder = new File(directory);
+    if (!folder.exists()) {
+      throw new IllegalArgumentException(String.format("Wrong path '%s'", directory));
     }
     final var files = new ArrayList<File>();
-    for (var path : directory.split(File.pathSeparator)) {
-      var file = new File(path);
-      if (file.isDirectory()) {
-        getFile(files, file);
-      } else {
-        files.add(file);
-      }
+    if (folder.isDirectory()) {
+      getFile(files, folder, deepSearch);
+    } else {
+      files.add(folder);
     }
     return Collections.unmodifiableList(files);
   }
 
-  private static void getFile(final List<File> files, final File folder) {
+  private static void getFile(final List<File> files, final File folder, final boolean deepSearch) {
     final var list = folder.listFiles();
     if (list != null) {
       for (var file : list) {
         if (file.isDirectory()) {
-          getFile(files, file);
+          if (deepSearch) {
+            getFile(files, file, true);
+          }
         } else {
           files.add(file);
         }
